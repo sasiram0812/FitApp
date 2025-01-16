b8f612054597bcfeda3e73603ae2655ba015ca01
Sasiram <v.sasirammanoj@gmail.com>
2025-01-16 19:51:56 +0530

    first commit

---
+++ script/Login.js
@@ -0,0 +1,24 @@
+let baseUrl=`https://good-tan-jay.cyclic.app/`
+
+
+let loginForm = document.querySelector('.login-wrap');
+let signupForm = document.querySelector('.signup-wrap');
+let title = document.querySelector('title');
+
+let signupToggleBtn = document.querySelector('#toggle-signup');
+let loginToggleBtn = document.querySelector('#toggle-login');
+let signupBtn=document.querySelector(".signup-btn");
+let loginBtn=document.querySelector(".login-btn");
+
+signupToggleBtn.onclick = () => {
+    loginForm.classList.remove('active');
+    signupForm.classList.add('active');
+    title.textContent = 'Signup form';
+}
+
+loginToggleBtn.onclick = () => {
+    signupForm.classList.remove('active');
+    loginForm.classList.add('active');
+    title.textContent = 'Login form';
+    alert("Sucessfully Login");
+}
